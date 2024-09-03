081669c06de4c79bef162e757bbf19d6adbb2a23
Gabriel Kruger <gabrielkr32@gmail.com>
2024-09-03 20:05:00 -0300

    Funcao atacar player

--- main.py
+++ main.py
@@ -7,21 +7,22 @@ player = {
     "nome": "Gabriel",
     "level": 1, 
     "exp": 0,
-    "exp_max": 50,
-    "ho": 100,
-    "ho_max": 100,
+    "exp_max": 30,
+    "hp": 100,
+    "hp_max": 100,
     "dano": 25,
 }
 
 
-def criar_npc():
-    level = randint(0,50)
+def criar_npc(level):
+    #level = randint(0,50)
 
     novo_npc = {
         "nome": f"Monstro #{level}",
         "level": level,
         "dano": 5 * level,
         "hp": 100 * level,
+        "hp_max": 100 * level,
         "exp": 7 * level,
     }
 
@@ -33,29 +34,40 @@ def gerar_npcs(n_npcs):
     
     for x in range (n_npcs):
         #print(x)
-        novo_npc = criar_npc(x + 1)#1+1 encremento
-        listas_npcs.append(novo_npc)
+        npc = criar_npc(x + 1)#encremento, faz a criação do npc começar no level 1, e nao 0.
+        listas_npcs.append(npc)
   
 
 def exibir_npcs():
     for npc in listas_npcs:
-        print(f"Nome: {npc['nome']}// Level: {npc['level']}// Dano: {npc['dano']}//HP:{npc['hp']}")
+        print(f"Nome: {npc['nome']}// Level: {npc['level']}// Dano: {npc['dano']}//HP:{npc['hp']}// EXP: {npc['exp']}")
        
+
+
 # atacar_npc(npc)>> npc:hp - player :dano
+def inicar_batalha(npc):
+    atacar_npc(npc)
+    atacar_player(npc)
+    exibir_info_batalha(npc)
+
 def atacar_npc(npc):
-    npc['hp'] =- player['dano']
+     npc['hp'] -= player['dano']    
 
 # atacar_player(npc)>> player:hp - npc:dano
+def atacar_player(npc):
+    player['hp'] -= npc['dano']
 
 
-
+def exibir_info_batalha(npc):
+    print(f"Player {player['hp']}/{player['hp_max']}")
+    print(f"NPC:{npc['nome']}: {npc['hp']}/{npc['hp_max']}")
 
 gerar_npcs(5)
-exibir_npcs()
+#exibir_npcs()
 
 npc_selecionado = listas_npcs[0]
 
-print("NPC selecionando:", npc_selecionado)
-atacar_npc(npc_selecionado)
+#print("NPC selecionando:", npc_selecionado)
+inicar_batalha(npc_selecionado)
 
-print("NPC atacado", npc_selecionado)
+#print("NPC atacado", npc_selecionado)
